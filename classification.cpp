8ec4950694dd661e279952c360b93e44f0d98ddd
Soumyadeep Dey <soumyadeep@localhost.localdomain>
2016-08-01 20:26:51 +0530

    1stAugustCommit

--- classification.cpp
+++ classification.cpp
@@ -98,6 +98,23 @@ void Training(char* TrainFile)
     trainClasses.release();
     ClassNumber.clear();
     
+
+#if _KNN_
+    
+    int K = 10;
+    #if defined HAVE_OPENCV_OCL && _OCL_KNN_
+      cv::ocl::KNearestNeighbour knnClassifier;
+      Mat temp, result;
+      knnClassifier.train(Data.TrainData, Data.TrainClass, temp, false, K);
+      cv::ocl::oclMat testSample_ocl, reslut_ocl;
+    #else
+      CvKNearest knnClassifier( Data.TrainData, Data.TrainClass, Mat(), false, K );
+    #endif
+      
+      knnClassifier.save("KNN_classifier.yml");
+    
+#endif
+    
 #if _NBC_
     
     CvNormalBayesClassifier normalBayesClassifier( Data.TrainData, Data.TrainClass );
@@ -155,9 +172,10 @@ void Training(char* TrainFile)
             svm_params.nu = 0.5;
             svm_params.p = 0;
             svm_params.term_crit = cvTermCriteria(CV_TERMCRIT_ITER, 1000, 0.01);
+	    
          // learn classifier
 #if defined HAVE_OPENCV_OCL && _OCL_SVM_
-    cv::ocl::CvSVM_OCL svmClassifier(trainSamples, trainClasses, Mat(), Mat(), params);
+    cv::ocl::CvSVM_OCL svmClassifier(trainSamples, trainClasses, Mat(), Mat(), svm_params);
 #else
     CvSVM svmClassifier( Data.TrainData, Data.TrainClass, Mat(), Mat(), svm_params );
 #endif
@@ -217,3 +235,195 @@ void Training(char* TrainFile)
 }
 
 
+#if _KNN_
+
+void classify_KNN(vector<SB> &blocks)
+{
+  #if defined HAVE_OPENCV_OCL && _OCL_KNN_
+      cv::ocl::KNearestNeighbour knnClassifier;
+      Mat temp, result;
+      knnClassifier.load("KNN_classifier.yml");
+      cv::ocl::oclMat testSample_ocl, reslut_ocl;
+    #else
+      CvKNearest knnClassifier;
+      knnClassifier.load("KNN_classifier.yml");
+    #endif
+      
+      for(int i=0;i<blocks.size();i++)
+    {
+      SB B = blocks[i];
+      B.FeatureVec;
+      Mat TestData;
+      //Mat(elem).copyTo(TestData);
+      Mat(B.FeatureVec).copyTo(TestData);
+      //printf("Rows = %d Cols = %d channel = %d\n",TestData.rows,TestData.cols,TestData.channels());
+      transpose(TestData,TestData);
+      int response;
+      
+      #if defined HAVE_OPENCV_OCL && _OCL_KNN_
+	  
+	    testSample_ocl.upload(TestData);
+	    int K = knnClassifier.get_max_k();
+	    knnClassifier.find_nearest(testSample_ocl, K, reslut_ocl);
+
+	    reslut_ocl.download(result);
+	    response = saturate_cast<int>(result.at<float>(0));
+	  
+	  #else
+	    
+	     int K = knnClassifier.get_max_k();
+	     response = (int)knnClassifier.find_nearest( TestData, K );
+	    
+	  #endif
+      
+      B.PredictedClass = response;
+      blocks[i] = B;
+    }
+}
+
+
+#endif
+
+#if _SVM_
+
+void classify_SVM(vector<SB> &blocks)
+{
+  
+
+
+#if defined HAVE_OPENCV_OCL && _OCL_SVM_
+    cv::ocl::CvSVM_OCL svmClassifier;
+#else
+    CvSVM svmClassifier;
+#endif 
+    
+    svmClassifier.load("SVM_Classifier.yml");
+    
+    for(int i=0;i<blocks.size();i++)
+    {
+      SB B = blocks[i];
+      B.FeatureVec;
+      Mat TestData;
+      //Mat(elem).copyTo(TestData);
+      Mat(B.FeatureVec).copyTo(TestData);
+      //printf("Rows = %d Cols = %d channel = %d\n",TestData.rows,TestData.cols,TestData.channels());
+      transpose(TestData,TestData);
+      int response = (int)svmClassifier.predict( TestData );
+      B.PredictedClass = response;
+      blocks[i] = B;
+    }
+  
+}
+
+    
+#endif
+
+#if _RF_
+
+void classify_RF(vector<SB> &blocks)
+{
+  CvRTrees  rtrees;
+  rtrees.load("RF_classifier.yml");
+  
+  for(int i=0;i<blocks.size();i++)
+    {
+      SB B = blocks[i];
+      B.FeatureVec;
+      Mat TestData;
+      //Mat(elem).copyTo(TestData);
+      Mat(B.FeatureVec).copyTo(TestData);
+      //printf("Rows = %d Cols = %d channel = %d\n",TestData.rows,TestData.cols,TestData.channels());
+      transpose(TestData,TestData);
+      int response = (int)rtrees.predict( TestData );
+      B.PredictedClass = response;
+      blocks[i] = B;
+    }
+  
+}
+
+#endif
+
+
+#if _DT_
+
+void classify_DT(vector<SB> &blocks)
+{
+  CvDTree  dtree;
+  dtree.load("DT_classifier.yml");
+  
+  for(int i=0;i<blocks.size();i++)
+    {
+      SB B = blocks[i];
+      B.FeatureVec;
+      Mat TestData;
+      //Mat(elem).copyTo(TestData);
+      Mat(B.FeatureVec).copyTo(TestData);
+      //printf("Rows = %d Cols = %d channel = %d\n",TestData.rows,TestData.cols,TestData.channels());
+      transpose(TestData,TestData);
+      int response = (int)dtree.predict( TestData );
+      B.PredictedClass = response;
+      blocks[i] = B;
+    }
+  
+}
+
+#endif
+
+#if _NBC_
+
+void classify_NBC(vector<SB> &blocks)
+{
+  CvNormalBayesClassifier normalBayesClassifier;
+  normalBayesClassifier.load("normalBayesClassifier.yml");
+  
+  for(int i=0;i<blocks.size();i++)
+    {
+      SB B = blocks[i];
+      B.FeatureVec;
+      Mat TestData;
+      //Mat(elem).copyTo(TestData);
+      Mat(B.FeatureVec).copyTo(TestData);
+      //printf("Rows = %d Cols = %d channel = %d\n",TestData.rows,TestData.cols,TestData.channels());
+      transpose(TestData,TestData);
+      int response = (int)normalBayesClassifier.predict( TestData );
+      B.PredictedClass = response;
+      blocks[i] = B;
+    }
+}
+
+
+#endif
+
+
+void Classification(vector<SB> &blocks, char *ClassifierName)
+{
+  if(ClassifierName == "NBC")
+  {
+    classify_NBC(blocks);
+  }
+  
+  if(ClassifierName == "KNN")
+  {
+    classify_KNN(blocks);
+  }
+  
+//   if(ClassifierName == "EM")
+//   {
+//     segmentation_withEM(PB,Data);
+//   }
+  
+  if(ClassifierName == "DT")
+  {
+    classify_DT(blocks);
+  }
+  
+  if(ClassifierName == "RF")
+  {
+    classify_RF(blocks);
+  }
+  
+  if(ClassifierName == "SVM")
+  {
+    classify_SVM(blocks);
+  }
+}
--- classification.h
+++ classification.h
@@ -10,24 +10,24 @@
 
 
 #ifdef HAVE_OPENCV_OCL
-#define _OCL_KNN_ 0 // select whether using ocl::KNN method or not, default is using
-#define _OCL_SVM_ 0 // select whether using ocl::svm method or not, default is using
+#define _OCL_KNN_ 1 // select whether using ocl::KNN method or not, default is using
+#define _OCL_SVM_ 1 // select whether using ocl::svm method or not, default is using
 #include "opencv2/ocl/ocl.hpp"
 #endif
 
 
 
 
-#define _NBC_ 1 // normal Bayessian classifier
+#define _NBC_ 0 // normal Bayessian classifier
 #define _KNN_ 1 // k nearest neighbors classifier
 #define _SVM_ 1 // support vectors machine
-#define _DT_  1 // decision tree
+#define _DT_  0 // decision tree
 #define _BT_  0 // ADA Boost
 #define _GBT_ 0 // gradient boosted trees
 #define _RF_  1 // random forest
 #define _ERT_ 0 // extremely randomized trees
 #define _ANN_ 0 // artificial neural networks
-#define _EM_  1 // expectation-maximization
+#define _EM_  0 // expectation-maximization
 
 
 typedef struct TrainDataClass
@@ -40,5 +40,17 @@ typedef struct TrainDataClass
 
 
 void Training(char *TrainFile);
+
+void classify_KNN(vector<SB> &blocks);
+
+void classify_NBC(vector<SB> &blocks);
+
+void classify_DT(vector<SB> &blocks);
+
+void classify_RF(vector<SB> &blocks);
+
+void classify_SVM(vector<SB> &blocks);
+
+void Classification(vector<SB> &blocks, char *ClassifierName);
 
 #endif
